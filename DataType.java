6f1e1b9b2f32bfd0165e85f344d2adf6cc08517f
vomeoyibu! <vomeoyibu1@gmail.com>
2025-03-19 12:52:40 +0000

    primitive type

---
+++ DataType.java
@@ -0,0 +1,34 @@
+public class DataType{
+    public static void main(String[] args){
+	    //Integer type(byte,short,int,long)
+		
+		byte age = 50;
+		System.out.printf("Your age is %d%n",age);
+		
+		short quantity = 32000;
+		System.out.printf("I bought %d pieces of bags for my business%n",quantity);
+		
+		int populationInNigeria = 1000000000;
+		System.out.printf("The total population in Nigeria is %d%n", populationInNigeria);
+		
+		long worldPopulation = 8000000000L;
+		System.out.printf("The world population is %d%n",worldPopulation);
+		
+		//float-point type(float,double)
+		float myBalance = 12345.858577F;
+		System.out.printf("Your account balance is %f%n",myBalance);
+		
+		double cbnReserve = 5575857.7373673636363;
+		System.out.printf("The CBN balance is %.5f%n",cbnReserve);
+		
+		//Boolean type (boolean)
+		boolean isJavaFun = true;
+		System.out.printf("Is Java a fun language ? %b%n",isJavaFun);
+		
+		//Character type(char)
+		char alpha = '@';
+		System.out.printf("My email is mercyben405%cgmail.com",alpha);
+		
+		
+	}
+}
